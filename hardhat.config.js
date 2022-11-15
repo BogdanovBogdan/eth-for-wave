b19970463999f80eda7c9d5bb0d209df1710b65b
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2022-11-15 07:32:33 +0400

    Add config of hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.17',
+  networks: {
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_URL,
+      accounts: [process.env.MM_GOERLI_PRIVATE_KEY],
+    },
+  },
 };
